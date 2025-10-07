14af217a99270e07bd4e0ec1ca996574623c2df7
Vishal Yarabandi <33473675+vishaljnv@users.noreply.github.com>
2025-10-07 11:23:28 +0530

    Search In Array of Unknown Length Complete

--- SearchInInfinteArray.cpp
+++ SearchInInfinteArray.cpp
@@ -1,37 +1,33 @@
- /*
-  * We are doing Binary Search here.
-  * Time Complexity   : O(log(n)) - We take a few iterations initially to find the sub-array to search in.
-  *                                 Then we do Binary Search. So we would take O(log(n)) interations to find the sub-array.
-  *                                 And then O(log(m)) to search. m being number of elements in the sub-array.
-  *                                 So, on average we can say that the time complexity if O(log(n)).
-  * Space Complexity  : O(1)      - No extra space is being used 
-  */
-int search(const ArrayReader& reader, int target)
-{
-     int low = 0, high = 1;
-     while (target > reader.get(high)) {
-         low = high;
-         high *= 2;
-     }
-        
-     return binarySearch(reader, target, low, high);
-}
-  
-int binarySearch(const ArrayReader& reader, int target, int low, int high)
-{
-    int mid;
-    while (low <= high) {
-        mid = (low + (high - low) / 2);
-        if (reader.get(mid) == target) {
-            return mid;
+class Solution {
+public:
+    int search(const ArrayReader& reader, int target)
+    {
+        int low = 0, high = 1;
+
+        while (reader.get(high) != INT_MAX) {
+            if ((reader.get(low) <= target) && (reader.get(high) >= target)) {
+                break;
+            }
+
+            low = high;
+            high = high * 2;
         }
-            
-        if (target > reader.get(mid)) {
-            low = mid + 1;
-        } else {
-            high = mid - 1;
+    
+        int mid = low + (high - low) / 2;
+        while (low <= high) {
+            if (reader.get(mid) == target) {
+                return mid;
+            }
+
+            if (reader.get(mid) < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+
+            mid = low + (high - low) / 2;
         }
+
+        return -1;
     }
-        
-    return -1;
-}
+};
